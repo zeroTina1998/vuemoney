0d450c199eecafbfb5ba8f007bb0465d75844732
Tina <1098299287@qq.com>
2021-04-20 15:28:16 +0800

    使用二维码进行扫描

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,32 @@
-import Vue from 'vue';
-import App from './App.vue';
-import './registerServiceWorker';
-import router from './router';
-import store from './store';
-import Nav from '@/components/Nav.vue';
-import Layout from '@/components/Layout.vue';
-import Icon from '@/components/Icon.vue';
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import Nav from "@/components/Nav.vue";
+import Layout from "@/components/Layout.vue";
+import Icon from "@/components/Icon.vue";
 
 Vue.config.productionTip = false;
 
-Vue.component('Nav', Nav);
-Vue.component('Layout', Layout);
-Vue.component('Icon', Icon);
+Vue.component("Nav", Nav);
+Vue.component("Layout", Layout);
+Vue.component("Icon", Icon);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app');
+  render: (h) => h(App),
+}).$mount("#app");
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert("请使用手机打开本页面，以保证浏览效果");
+  const img = document.createElement("img");
+  img.src = "/qrcode.png";
+  img.style.position = "fixed";
+  img.style.top = "50%";
+  img.style.left = "50%";
+  img.style.transform = "translate(-50%,-50%)";
+  img.style.boxShadow = "0 0 10px rgba(0,0,0,0.25)";
+  document.body.appendChild(img);
+}
--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,31 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-money-2/" : "/",
   lintOnSave: false,
-  chainWebpack: config =>{
-    const dir = path.resolve(__dirname, 'src/assets/icons')
+  chainWebpack: (config) => {
+    const dir = path.resolve(__dirname, "src/assets/icons");
 
     config.module
-      .rule('svg-sprite')
+      .rule("svg-sprite")
       .test(/\.svg$/)
-      .include.add(dir).end() // 包含 icons 目录
-      .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract:false}).end()
-      .use('svgo-loader').loader('svgo-loader')
-      .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
-    config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
-    config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
-
+      .include.add(dir)
+      .end() // 包含 icons 目录
+      .use("svg-sprite-loader-mod")
+      .loader("svg-sprite-loader-mod")
+      .options({ extract: false })
+      .end()
+      .use("svgo-loader")
+      .loader("svgo-loader")
+      .tap((options) => ({
+        ...options,
+        plugins: [{ removeAttrs: { attrs: "fill" } }],
+      }))
+      .end();
+    config
+      .plugin("svg-sprite")
+      .use(require("svg-sprite-loader-mod/plugin"), [{ plainSprite: true }]);
+    config.module.rule("svg").exclude.add(dir); // 其他 svg loader 排除 icons 目录
 
     // config.module
     //   .rule('svg-sprite')
@@ -26,6 +37,5 @@ module.exports = {
     //   .end()
     // config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
     // config.module.rule('svg').exclude.add(dir)
-
-  }
-}
+  },
+};
